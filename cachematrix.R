9baaad019321859defbbaad9b2684762f38ee4cb
charulverma <charul.verma2017@sims.edu>
2016-09-11 13:25:08 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Matrix inversion is usually a costly computation and there may be some benefit
+# to caching the inverse of a matrix rather than compute it repeatedly. The
+# following two functions are used to cache the inverse of a matrix.
+>   makeCacheMatrix <- function(x = matrix()) {
++     inv <- NULL
++      set <- function(y) {
++          x <<- y
++          inv <<- NULL
++      }
++      get <- function() x
++      setinverse <- function(inverse) inv <<- inverse
++      getinverse <- function() inv
++      list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
++  }
+>  
+# This function assumes that the matrix is always invertible.
+>  cacheSolve <- function(x, ...) {
++      inv <- x$getinverse()
++      if(!is.null(inv)) {
++          message("getting cached data.")
++          return(inv)
++      }
++      data <- x$get()
++      inv <- solve(data)
++      x$setinverse(inv)
++      inv
++  }
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# Sample Run
+> x = rbind(c(4,3), c(3, 2))
+>  m = makeCacheMatrix(x)
+>  m$get()
+     [,1] [,2]
+[1,]    4    3
+[2,]    3    2
+> cacheSolve(m)
+     [,1] [,2]
+[1,]   -2    3
+[2,]    3   -4
+> cacheSolve(m)
+getting cached data.
+     [,1] [,2]
+[1,]   -2    3
+[2,]    3   -4
+> 
